159f8a067c5a28ec91309a4e860df6aa508fe9d6
ailuschenko <iluschenko@nwtn-group.com>
2022-05-02 14:09:33 +0500

    добавил видосики, картинки, аллюр1

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
