d37fc972266bc341c67a797ad0a4dd222593ca55
ailuschenko <iluschenko@nwtn-group.com>
2022-05-02 12:58:17 +0500

    добавил видосики, картинки, аллюр

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -11,7 +11,6 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
-        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         DesiredCapabilities capabilities = new DesiredCapabilities();
